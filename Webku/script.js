62a0daf0d8fb41501d6a761426f0e1212a8753cd
Vito Zharfan Syarif Prayogo <92035563+radenmasvito@users.noreply.github.com>
2022-01-20 16:11:21 +0700

    Add files via upload

---
+++ Webku/script.js
@@ -0,0 +1,8 @@
+alert('SELAMAT DATANG DI WEBSITE KAMI');
+var yakin = confirm("Silahkan bagi yang mau berkunjung ke instagram saya");
+    if (yakin) {
+        window.location = "https://instagram.com/vitozharfan___";
+    } 
+    else {
+        document.write("Nanti ya, habis belanja accessoris hp dulu");
+    }
